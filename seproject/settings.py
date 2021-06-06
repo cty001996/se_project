d7310807a600baddb415258c5cd6b4a217280acc
張定堯 <cty001996@gmail.com>
2021-06-06 18:02:00 +0800

    add procfile

--- seproject/settings.py
+++ seproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -167,3 +168,6 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "ntuonlinegroup@gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD = "HZEDtPTNNvWQRwug"
+
+
+django_heroku.settings(locals())
