3471d4f43e3b0f5b31acc24bb8e23ca4823aac62
張定堯 <cty001996@gmail.com>
2021-06-06 18:54:06 +0800

    test deploy5

--- seproject/settings.py
+++ seproject/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # https://pypi.org/project/django-cors-headers/
